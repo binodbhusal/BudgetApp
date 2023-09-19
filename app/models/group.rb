dafd283770a81e9be78ccd1e49db87ec4164f767
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-09-19 17:50:53 +0100

    Add group model

---
+++ app/models/group.rb
@@ -0,0 +1,7 @@
+class Group < ApplicationRecord
+  belongs_to :user
+  has_and_belongs_to_many :records
+  validates :name, presence: true, length: { maximum: 70 }
+  validates :icon, presence: true
+  validates_uniqueness_of :name, scope: :user_id
+end
