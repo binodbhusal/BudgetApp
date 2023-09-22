eef7911f0283896726f46e6f25780acc0e58393a
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-09-22 12:57:26 +0100

    Update group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -4,4 +4,8 @@ class Group < ApplicationRecord
   validates :name, presence: true, length: { maximum: 70 }
   validates :icon, presence: true
   validates_uniqueness_of :name, scope: :user_id
+
+  def total_amount
+    records.sum(:amount)
+  end
 end
